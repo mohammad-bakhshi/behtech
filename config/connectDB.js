a3957893ee883d14777e54206eb5fe1f4f9af6b5
mohammad-bakhshi <mohammadbakhshi1979@gmail.com>
2025-12-07 19:58:13 +0330

    create connection to mongo db

---
+++ config/connectDB.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+export const connectDB = async (url, dbName) => {
+  if (!url) {
+    console.log("Please provide a database URI string");
+    process.exit(1);
+  }
+  try {
+    await mongoose.connect(url, { dbName });
+    console.log("Database connection established!");
+  } catch (error) {
+    console.error(`Database Connection Failed: ${error.message}`);
+    process.exit(1);
+  }
+};
