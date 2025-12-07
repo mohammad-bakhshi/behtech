6cb96618d4cd10577e3037bb28468f355b2cd9bb
mohammad-bakhshi <mohammadbakhshi1979@gmail.com>
2025-12-07 19:57:37 +0330

    add connection to db and connect to routers

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const express = require("express");
-const { config } = require("dotenv");
+import express from "express";
+import { config } from "dotenv";
+import { connectDB } from "./config/connectDB.js";
+import ProductRouter from "./routes/product.router.js";
 
 config();
 const app = express();
@@ -8,12 +10,42 @@ const port = process.env.PORT || 3000;
 // Middleware to parse JSON bodies
 app.use(express.json());
 
-// Sample route
+app.use("/api", ProductRouter);
+
+// Health check endpoint
+
 app.get("/", (req, res) => {
-  res.send("Hello, World!");
+  res.send({
+    status: 200,
+    success: true,
+    message: "Server Running...",
+  });
+});
+
+// Handle 404 errors
+app.use((req, res, next) => {
+  const error = new Error("Requested URL Not Found");
+  error.status = 404;
+  next(error);
+});
+
+// Global error handling middleware
+app.use((error, req, res, next) => {
+  res.status(error.status || 500).send({
+    status: error.status || 500,
+    success: false,
+    message: error.message || "Internal Server Error",
+  });
 });
 
 // Start the server
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+// Database connection and server start
+(async () => {
+  try {
+    await connectDB(process.env.DB_URI, process.env.DB_NAME);
+    app.listen(port, () => console.log("APP RUNNING ON PORT:", port));
+  } catch (error) {
+    console.error("Failed to Start the Server: ", error);
+    process.exit(1);
+  }
+})();
