de28ce78c428173c28cb69a926edc2e4225e1b6e
mohammad-bakhshi <mohammadbakhshi1979@gmail.com>
2025-12-08 21:51:09 +0330

    fix jalali to iso function

--- utils/jalali-to-iso.js
+++ utils/jalali-to-iso.js
@@ -1,4 +1,4 @@
-import moment from "moment";
+import moment from "moment-jalaali";
 
 export const convertJalaliToISO = (jalaliDateString) => {
   const m = moment(jalaliDateString, "jYYYY/jMM/jDD", "fa");
