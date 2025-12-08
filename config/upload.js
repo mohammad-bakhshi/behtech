59fe0dde7a8a7c30056231b4a28ff80cef72b8c8
mohammad-bakhshi <mohammadbakhshi1979@gmail.com>
2025-12-08 22:34:21 +0330

    fix comments

--- config/upload.js
+++ config/upload.js
@@ -4,11 +4,9 @@ import path from "path";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    // Specify the directory where files should be stored (e.g., a 'uploads' folder)
     cb(null, "uploads/");
   },
   filename: function (req, file, cb) {
-    // Naming convention: fieldname-timestamp.ext
     cb(
       null,
       file.fieldname + "-" + Date.now() + path.extname(file.originalname)
@@ -22,10 +20,8 @@ if (!fs.existsSync("uploads")) {
 
 export const upload = multer({
   storage: storage,
-  // Optional: Limit file size and type
-  limits: { fileSize: 10 * 1024 * 1024 }, // 5MB limit
+  limits: { fileSize: 10 * 1024 * 1024 }, // 10MB limit
   fileFilter: (req, file, cb) => {
-    // Only accept Excel files (.xlsx or .xls)
     if (
       file.mimetype ===
         "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" ||
